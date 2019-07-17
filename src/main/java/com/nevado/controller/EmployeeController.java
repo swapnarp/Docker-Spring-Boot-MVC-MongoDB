758acfdaa4a72ee40cc62a14283a2adcca2679d9
sparuchuri <sparuchuri@lifetouch.com>
2019-07-16 21:55:51 -0500

    commit

---
+++ src/main/java/com/nevado/controller/EmployeeController.java
@@ -0,0 +1,46 @@
+package com.nevado.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nevado.model.Employee;
+import com.nevado.persistence.EmployeeRepository;
+
+@Component
+@Configuration
+@RestController
+public class EmployeeController {
+
+
+	@Value("${application.message}")
+	String message;
+
+	@Value("${application.appname}")
+	String appname;
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!- " + message + " " + appname;
+	}
+
+	@RequestMapping(value = "/employee", method = RequestMethod.POST)
+	public Employee create(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/{employeeId}")
+	public Employee get(@PathVariable Employee employee) {
+		return employeeRepository.insert(employee);
+
+	}
+}
